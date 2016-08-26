0a07081bc53756bd858c150dd05afd9a803afd6a
Arne Maier <Arne_Maier@gmx.de>
2016-08-26 16:04:27 +0200

    Add vim with brew

--- setUp.sh
+++ setUp.sh
@@ -1,5 +1,12 @@
 #/bin/sh
 
+# zsh
+cat ~/.oh-my-zsh/templates/zshrc.zsh-template > ~/.zshrc
+echo "alias vi='vim'" >> ~/.zshrc 
+
 # vim
+brew install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cat ./vim-plug.config >> ~/.vimrc
+
+
