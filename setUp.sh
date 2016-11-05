90d2d3f6e76ee657d6f8ddd441591085bb253eb4
Arne Maier <Arne_Maier@gmx.de>
2016-11-05 12:12:06 +0100

    Added nvm

--- setUp.sh
+++ setUp.sh
@@ -1,13 +1,23 @@
 #/bin/sh
 
+# brew
+
+brew install nvm
+
+# structure
+mkdir -p ~/.nvm
+
 # zsh
 cat ~/.oh-my-zsh/templates/zshrc.zsh-template > ~/.zshrc
 echo "alias vi='vim'" >> ~/.zshrc 
 echo "alias serve=\"python -m SimpleHTTPServer 8000\"" >> ~/.zshrc
+echo "export NVM_DIR=~/.nvm" >> "/.zshrc
+echo source $(brew --prefix nvm)/nvm.sh >> ~/.zshrc
+
 
 # vim
 brew install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cat ./vimrc >> ~/.vimrc
+cat ./vimrc > ~/.vimrc
 
 
